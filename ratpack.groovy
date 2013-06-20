1a1dea30c673bb67d499c0fd93e7872038dd53a8
Luke Daley <ld@ldaley.com>
2013-06-20 19:56:43 +0100

    Clean up whitespace.

--- ratpack.groovy
+++ ratpack.groovy
@@ -6,27 +6,24 @@ import static org.ratpackframework.groovy.RatpackScript.ratpack
 
 ratpack {
     handlers {
-	
-	    // Only initialize this once
-		def util = new TarotDeckUtil()
+        // Only initialize this once
+        def util = new TarotDeckUtil()
 
-		// default route
-        get {		
-            get(TemplateRenderer).render "index.html", title: "Groovy Tarot Caster"		
+        // default route
+        get {
+            get(TemplateRenderer).render "index.html", title: "Groovy Tarot Caster"
         }
-		
-		// Actual dynamic part
-		get ("tarot") {
-			def card = util.randomCard()
-            get(TemplateRenderer).render "tarot.html", name: card.name, meaning: card.meaning, image: "images\\$card.image"						
-        }		
-		
-		get ("about") {
-            get(TemplateRenderer).render "about.html", title: "Groovy Tarot Caster"		
-		}
+
+        // Actual dynamic part
+        get ("tarot") {
+            def card = util.randomCard()
+            get(TemplateRenderer).render "tarot.html", name: card.name, meaning: card.meaning, image: "images\\$card.image"
+        }
+
+        get ("about") {
+            get(TemplateRenderer).render "about.html", title: "Groovy Tarot Caster"
+        }
 
         assets "public"
     }
 }
-
-
